cb9d4139e31a3f214b3708f55fd48aee539617be
Paul Mould <Paul.Mould@coxautoinc.com>
2023-05-25 23:56:09 -0500

    updates

--- src/main.ts
+++ src/main.ts
@@ -15,9 +15,17 @@ import * as core from '@actions/core'
 //     if (error instanceof Error) core.setFailed(error.message)
 //   }
 // }
+import * as event from './event'
+import * as version from './version'
 
 export async function run(): Promise<void> {
   try {
+    const tag = event.getCreatedTag()
+
+    if (tag && version.isSemVer(tag)) {
+      // TODO
+    }
+
     core.setOutput('release-url', 'https://example/com')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
